dfe9486ba946e77a693b5075e69eeeff75d4b901
Rob Quinn <Rob.M.Quinn@gmail.com>
2019-12-15 17:08:04 -0500

    got latitude and longitue from current service to use with UV

--- main.js
+++ main.js
@@ -1,66 +1,101 @@
 $(document).ready(function(){
 
-$("#submitWeather").click(function(){
-
-    let apiKey = "43604669ed26bcf8b5858af9ce91c46a";
-    // var city = $("#city").val();
-    var city = "austin";
-    let units = "imperial";
-    let urlCurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&appid=" + apiKey;
-    let urlFive = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=" + units + "&appid=" + apiKey;
+    $("#submitWeather").click(function(){
     
+        let apiKey = "43604669ed26bcf8b5858af9ce91c46a";
+        // var city = $("#city").val();
+        var city = "austin";
+        let units = "imperial";
+        let days = "5;"
+        let urlCurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&appid=" + apiKey;
+        let urlFive = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=" + units + "&appid=" + apiKey;
+        let urlUV = "http://api.openweathermap.org/data/2.5/uvi?q=" + city + "&appid=" + apiKey;
+
+        console.log(urlUV);
+        
+        
+            // var queryURL_5Day = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=43604669ed26bcf8b5858af9ce91c46a";
+                
+            //     var queryURL_5Day = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=43604669ed26bcf8b5858af9ce91c46a";
+                // "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startYear + "&end_date" + endYear + "&q=" + query + "&api-key=9qYOevLt8LZN1Y60GVBvkg1iZHfGFO2Z";
+               
+              if(city != ""){ 
+               
+               
+                $.ajax({
+                    url: urlCurrent,
+                    method: "GET",
+                    // dataType: "jsonp",
+                    success: function(current){
+                        var widget = show(current);
+                    $("#result").html(widget);
+                    var lon = current.coord.lon;
+                    var lat = current.coord.lat;
+                    console.log(lon);
     
-        // var queryURL_5Day = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=43604669ed26bcf8b5858af9ce91c46a";
-            
-        //     var queryURL_5Day = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=43604669ed26bcf8b5858af9ce91c46a";
-            // "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startYear + "&end_date" + endYear + "&q=" + query + "&api-key=9qYOevLt8LZN1Y60GVBvkg1iZHfGFO2Z";
-           
-          if(city != ""){ 
-           
-           
-            $.ajax({
-                url: urlCurrent,
-                method: "GET",
-                dataType: "jsonp",
-                success: function(data){
-                    var widget = show(data);
-                $("#result").html(widget);
+                    // $("#city").val("");
+                    }
+                }),
 
-                // $("#city").val("");
-                }
-            });
-
-        }else{
-
-            $("#error").html("Field cannot be empty");
-         }
-      
+                $.ajax({
+                    url: urlFive,
+                    method: "GET",
+                    // dataType: "jsonp",   
+                    success: function(five){
+                        var widget5 = show(five);
+                    $("#5dayForecast").html(widget5);
+                    console.log(five)
+                    // $("#city").val("");
+                    }
                 });
-               });
+         
+            
+                
+            }else{
     
-
-
-function show(data){
+                $("#error").html("Field cannot be empty");
+             }
+          
+                    });
+                   
+        
     
-  var currentDate = moment().format("dddd, MMM Do YYYY");
-  $("#today").text
-    (currentDate);
+    
+    function show(current){
+        
+      var currentDate = moment().format("dddd, MMM Do YYYY");
+      $("#today").text
+        (currentDate);
+    
+    return  "<h2>" + current.name +" ("+ currentDate+ ")" +"</h2>" +
+            "<h3><strong>Temperature</strong>: "+ current.main.temp + " °F" +"</h3>" +
+            "<h3><strong>Humidity</strong>: "+ current.main.humidity + "%" +"</h3>" + 
+            "<h3><strong>Wind Speed</strong>: "+ current.wind.speed + " MPH" +"</h3>"  
+         
+        };
+    
+        // function show(five){
+        
+        //     // console.log("test");
+        //     //    console.log(city);
+              
+        //   return 
+        //     //    "<h2>" + list[2].dt_txt +"</h2>" 
+        //         //   "<h3><strong>Temperature</strong>: "+ current.main.temp + " °F" +"</h3>" +
+        //         //   "<h3><strong>Humidity</strong>: "+ current.main.humidity + "%" +"</h3>" + 
+        //         //   "<h3><strong>Wind Speed</strong>: "+ current.wind.speed + " MPH" +"</h3>"  
+        // console.log("test");
+        //       };
+    // request(url, function (err, response, body) {
+    //   if(err){
+    //     console.log('error:', error);
+    //   } else {
+    //     console.log('body:', body);
+    //   }
+    // });
+    
+    
+    // "<h3><strong>Weather</strong>: "+ data.list[0].main.temp +"</h3>"  
+            // "<h3><strong>Description</strong>: "+ data.weather[0].description +"</h3>" +
 
-return  "<h2>" + data.name +" ("+ currentDate+ ")" +"</h2>" +
-        "<h3><strong>Temperature</strong>: "+ data.main.temp + " °F" +"</h3>" +
-        "<h3><strong>Humidity</strong>: "+ data.main.humidity + "%" +"</h3>" + 
-        "<h3><strong>Wind Speed</strong>: "+ data.wind.speed + " MPH" +"</h3>"  
-     
-    }
-
-// request(url, function (err, response, body) {
-//   if(err){
-//     console.log('error:', error);
-//   } else {
-//     console.log('body:', body);
-//   }
-// });
-
-
-// "<h3><strong>Weather</strong>: "+ data.list[0].main.temp +"</h3>"  
-        // "<h3><strong>Description</strong>: "+ data.weather[0].description +"</h3>" +
+        });
