45093df3a7ec4431da31ec5adb3aadeb34945365
Robert Quinn <robertquinn@Roberts-Mac-mini.local>
2019-12-16 23:38:29 -0500

    updated from hardcoded city to use city in search box

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ $(document).ready(function () {
     $("#submitWeather").click(function () {
 
         let apiKey = "43604669ed26bcf8b5858af9ce91c46a";
-        // var city = $("#city").val();
-        var city = "austin";
+        var city = $("#city").val();
+        // var city = "austin";
         let units = "imperial";
         let days = "5;"
         let urlCurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&appid=" + apiKey;
         let urlFive = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=" + units + "&appid=" + apiKey;
-        
 
+        console.log(city);
         if (city != "") {
 
-console.log(urlFive);
+            console.log(urlFive);
 
             $.ajax({
                 url: urlCurrent,
@@ -25,63 +25,63 @@ console.log(urlFive);
                     var lon = current.coord.lon;
                     var lat = current.coord.lat;
                     var urlUV = "http://api.openweathermap.org/data/2.5/uvi?q=&appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
-                  
+
                     function show(current) {
 
                         var currentDate = moment().format("dddd, MMM Do YYYY");
                         $("#today").text
                             (currentDate);
-                
+
                         return "<h2>" + current.name + " (" + currentDate + ")" + "</h2>" +
                             "<p><strong>Temperature</strong>: " + current.main.temp + " °F" + "</p>" +
                             "<p><strong>Humidity</strong>: " + current.main.humidity + "%" + "</p>" +
                             "<p><strong>Wind Speed</strong>: " + current.wind.speed + " MPH" + "</p>"
-                       
+
                     };
 
-            
-                   
+
+
                     $.ajax({
-                            url: urlUV,
-                            method: "GET",
-                                success: function (UV) {
-                                var ultraViolet = show(UV);
-                                $("#UV").html(ultraViolet);
-                               
-                                function show(UV) {
+                        url: urlUV,
+                        method: "GET",
+                        success: function (UV) {
+                            var ultraViolet = show(UV);
+                            $("#UV").html(ultraViolet);
+
+                            function show(UV) {
 
 
-                                    return "<p><strong>UV Index</strong>: " + UV.value + "</p>"
-                            
-                                };
+                                return "<p><strong>UV Index</strong>: " + UV.value + "</p>"
 
-  
-                         
+                            };
+
+
+
+                        }
+                    });
                 }
             });
-        }
-    }); 
 
 
 
-    $.ajax({
-        url: urlFive,
-        method: "GET",
-        // dataType: "jsonp",   
-        success: function(five){
-            var cards = show(five);
-        $("#day1").html(cards);
-        console.log(five)
-        // $("#city").val("");
+            $.ajax({
+                url: urlFive,
+                method: "GET",
+                // dataType: "jsonp",   
+                success: function (five) {
+                    var cards = show(five);
+                    $("#day1").html(cards);
+                    console.log(five)
+                    // $("#city").val("");
 
-        function show(five) {
+                    function show(five) {
 
 
-            return "<h3><strong></strong>" + five.list[0].dt_txt + "</h3>"
-    
-        };
-        }
-    });
+                        return "<h3><strong></strong>" + five.list[0].dt_txt + "</h3>"
+
+                    };
+                }
+            });
 
 
 
@@ -90,13 +90,13 @@ console.log(urlFive);
             $("#error").html("Field cannot be empty");
         }
 
-  
-
-  
 
 
 
-});
+
+
+
+    });
 
     // function show(five){
 
