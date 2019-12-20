22a3eddf34a004a21377a074d75c6561b66ab9f0
Rob Quinn <Rob.M.Quinn@gmail.com>
2019-12-19 23:28:37 -0500

    updated local storage on load and on click

--- main.js
+++ main.js
@@ -1,37 +1,137 @@
+
+
 $(document).ready(function () {
+
+     for (var i = 0; i < localStorage.length; i++) {
+        var currentCity = localStorage.getItem(localStorage.key(i));
+        $("#previousCity").prepend(
+          '<tr><td class="linkCity"><label> ' + currentCity + " </label></td></tr>"
+        );
+        };
+
+    // var currentCity = localStorage.getItem(localStorage.length-1);
+    // $("#previousCity").prepend(
+    //   '<tr><td class="linkCity"><label> ' + currentCity + " </label></td></tr>"
+    // );
+    
+
+var lastSearch =  localStorage.getItem(localStorage.length-1);
+console.log(lastSearch);
+let apiKey = "43604669ed26bcf8b5858af9ce91c46a";
+var city = lastSearch  
+        let units = "imperial";
+        let urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&appid=" + apiKey;
+        let urlFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=" + units + "&appid=" + apiKey;
+        
+
+        $.ajax({
+            url: urlCurrent,
+            method: "GET",
+            // dataType: "jsonp",
+            success: function (current) {
+                var widget = show(current);
+                $("#result").html(widget);
+                var lon = current.coord.lon;
+                var lat = current.coord.lat;
+                var urlUV = "https://api.openweathermap.org/data/2.5/uvi?q=&appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+
+                function show(current) {
+
+                    var currentDate = moment().format("dddd, MMM Do YYYY");
+                    $("#today").text
+                        (currentDate);
+
+                    return "<h2>" + current.name + " (" + currentDate + ")" + "</h2>" +
+                        "<p><strong>Temperature</strong>: " + current.main.temp + " °F" + "</p>" +
+                        "<p><strong>Humidity</strong>: " + current.main.humidity + "%" + "</p>" +
+                        "<p><strong>Wind Speed</strong>: " + current.wind.speed + " MPH" + "</p>"
+
+                };
+
+
+
+                $.ajax({
+                    url: urlUV,
+                    method: "GET",
+                    success: function (UV) {
+                        var ultraViolet = show(UV);
+                        $("#UV").html(ultraViolet);
+
+                        function show(UV) {
+
+
+                            return "<p><strong>UV Index</strong>: " + UV.value + "</p>"
+
+                        };
+
+
+
+                    }
+                });
+            }
+        });
+
+
+
+        $.ajax({
+            url: urlFive,
+            method: "GET",
+               success: function (five) {
+                var cards = show(five);
+              
+                function show(five) {
+
+                   
+                  return $("#day1").html(moment(five.list[7].dt_txt).format("ll")),
+                    $("#icon1").html('<img src="http://openweathermap.org/img/wn/' + five.list[7].weather[0].icon + '.png"/>'),
+                    $("#temp1").html("Temp: " + five.list[7].main.temp_max + " °F"),
+                    $("#humidity1").html("Humidity: " + five.list[7].main.humidity + "%"),
+                    $("#day2").html(moment(five.list[15].dt_txt).format("ll")),
+                    $("#icon2").html('<img src="http://openweathermap.org/img/wn/' + five.list[15].weather[0].icon + '.png"/>'),
+                    $("#temp2").html("Temp: " + five.list[15].main.temp_max + " °F"),
+                    $("#humidity2").html("Humidity: " + five.list[15].main.humidity + "%"),
+                    $("#day3").html(moment(five.list[23].dt_txt).format("ll")),
+                    $("#icon3").html('<img src="http://openweathermap.org/img/wn/' + five.list[23].weather[0].icon + '.png"/>'),
+                    $("#temp3").html("Temp: " + five.list[23].main.temp_max + " °F"),
+                    $("#humidity3").html("Humidity: " + five.list[23].main.humidity + "%"),
+                    $("#day4").html(moment(five.list[31].dt_txt).format("ll")),
+                    $("#icon4").html('<img src="http://openweathermap.org/img/wn/' + five.list[31].weather[0].icon + '.png"/>'),
+                    $("#temp4").html("Temp: " + five.list[31].main.temp_max + " °F"),
+                    $("#humidity4").html("Humidity: " + five.list[31].main.humidity + "%"),
+                    $("#day5").html(moment(five.list[39].dt_txt).format("ll")),
+                    $("#icon5").html('<img src="http://openweathermap.org/img/wn/' + five.list[39].weather[0].icon + '.png"/>'),
+                    $("#temp5").html("Temp: " + five.list[39].main.temp_max + " °F"),
+                    $("#humidity5").html("Humidity: " + five.list[39].main.humidity + "%")
+
+                };
+            }
+        });
+   
+
+
+   
 
     $("#submitWeather").click(function () {
 
         let apiKey = "43604669ed26bcf8b5858af9ce91c46a";
         var city = $("#city").val();
-        // var city = "austin";
         let units = "imperial";
-        // let days = "5;"
-        let urlCurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&appid=" + apiKey;
-        let urlFive = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=" + units + "&appid=" + apiKey;
+        let urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&appid=" + apiKey;
+        let urlFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=" + units + "&appid=" + apiKey;
         var hist = localStorage.length;
-        console.log(hist);
         localStorage.setItem(hist, city);
-        const search = JSON.parse(localStorage.getItem("city")) || [];
-
-const maxSearch = 5;
         
-
-        console.log(search);
         if (city != "") {
-
-            console.log(urlFive);
 
             $.ajax({
                 url: urlCurrent,
                 method: "GET",
-                // dataType: "jsonp",
-                success: function (current) {
+                  success: function (current) {
                     var widget = show(current);
                     $("#result").html(widget);
                     var lon = current.coord.lon;
                     var lat = current.coord.lat;
-                    var urlUV = "http://api.openweathermap.org/data/2.5/uvi?q=&appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+                    var urlUV = "https://api.openweathermap.org/data/2.5/uvi?q=&appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
 
                     function show(current) {
 
@@ -74,13 +174,9 @@ const maxSearch = 5;
             $.ajax({
                 url: urlFive,
                 method: "GET",
-                // dataType: "jsonp",   
                 success: function (five) {
                     var cards = show(five);
-                    // $(".card").html(cards);
-                    console.log(five.list[7].dt_txt)
-                    // $("#city").val("");
-
+                 
                     function show(five) {
 
                        
@@ -117,30 +213,24 @@ const maxSearch = 5;
         }
 
 
-     
+        
+        var newCity = localStorage.getItem(localStorage.length-1);
+    $("#previousCity").prepend(
+      '<tr><td class="linkCity"><label> ' + newCity + " </label></td></tr>"
+    );
+
 
     });
 
+    $(".linkCity").click(function() {
+        var tr = $(this)
+          .closest("tr")
+          .find("label")
+          .text();
 
-    var hist1 = (localStorage.getItem("0")) || [];
-    var hist2 = (localStorage.getItem("1")) || [];
-    var hist3 = (localStorage.getItem("2")) || [];
-    var hist4 = (localStorage.getItem("3")) || [];
-    var hist5 = (localStorage.getItem("4")) || [];
-    var hist6 = (localStorage.getItem("5")) || [];
-    var hist7 = (localStorage.getItem("6")) || [];
-    var hist8 = (localStorage.getItem("7")) || [];
-    var hist9 = (localStorage.getItem("8")) || [];
-  
+          console.log(tr);
+    });
 
-    $("#hist-9").text(hist1);
-    $("#hist-8").html(hist2);
-    $("#hist-7").html(hist3);
-    $("#hist-6").html(hist4);
-    $("#hist-5").html(hist5);
-    $("#hist-4").html(hist6);
-    $("#hist-3").html(hist7);
-    $("#hist-2").html(hist8);
-    $("#hist-1").html(hist9);
+
 
 });
